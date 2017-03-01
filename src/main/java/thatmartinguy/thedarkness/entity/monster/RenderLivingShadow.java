b302f03166c1a7d698092850c8fd486145cbaf61
adudewithapc <axel.holm.mc@hotmail.com>
2017-03-01 19:04:31 +0100

    Fixed living shadow not attacking the player

--- src/main/java/thatmartinguy/thedarkness/entity/monster/RenderLivingShadow.java
+++ src/main/java/thatmartinguy/thedarkness/entity/monster/RenderLivingShadow.java
@@ -1,25 +1,23 @@
 package thatmartinguy.thedarkness.entity.monster;
 
-import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.renderer.entity.RenderBiped;
-import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 import thatmartinguy.thedarkness.util.Reference;
 
 public class RenderLivingShadow extends RenderBiped<EntityLivingShadow>
 {
-	private static final ResourceLocation LIVING_SHADOW_TEXTURE = new ResourceLocation(Reference.MOD_ID, "textures/entity/livingshadow.png");
+	private final ResourceLocation TEXTURE_LOCATION = new ResourceLocation(Reference.MOD_ID, "textures/entity/livingshadow.png");
 	
-	public RenderLivingShadow(RenderManager rendermanagerIn)
+	public RenderLivingShadow(RenderManager renderManagerIn)
 	{
-		super(rendermanagerIn, new ModelBiped(), 0);
+		super(renderManagerIn, new ModelBiped(), 0);
 	}
-
+	
 	@Override
 	protected ResourceLocation getEntityTexture(EntityLivingShadow entity)
 	{
-		return LIVING_SHADOW_TEXTURE;
+		return TEXTURE_LOCATION;
 	}
 }
